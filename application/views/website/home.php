ff8d4d98b85802080cb936999bd4fcd230b37221
BikTh <thierrybikoko8@gmail.com>
2022-08-17 16:43:02 +0100

    update

--- application/views/website/home.php
+++ application/views/website/home.php
@@ -1,5 +1,6 @@
-<section class="bg-beige">
-    <div class="container">
-        sdgdthtr
+<section class="">
+    <div class="row m-0">
+        <div class="col-5 bg-koki1"></div>
+        <div class="col-7"></div>
     </div>
 </section>
